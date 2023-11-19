584aa451bc599da6893546c3fbc6a1c7fa03b5b2
jungesla <jungeslarissajunges@gmail.com>
2023-11-19 17:58:02 -0300

    Filtros requisicao

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,7 +3,9 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -14,9 +16,14 @@ import java.util.Date;
 @Service
 public class TokenService {
 
+
+    @Value("{api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario){
+        System.out.println(secret);
         try {
-            var algoritmo = Algorithm.HMAC256("123456");
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API voll.med")
                     .withSubject(usuario.getLogin())
@@ -24,7 +31,22 @@ public class TokenService {
                     .withClaim("id", usuario.getId())
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
-           throw new RuntimeException("erro ap gerar token", exception);
+           throw new RuntimeException("erro ao gerar token", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+               return JWT.require(algoritmo)
+                    .withIssuer("API voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+
+        } catch (JWTVerificationException exception){
+           throw new RuntimeException("Token inválido ou expirado");
         }
     }
 
